d691e953cbf01ac0641799bdb4810d425a322168
Rei <you@example.com>
2021-11-14 16:32:23 +0300

    add mobile navigation

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Menu from '../components/sidebar/Menu'
+import MobileNav from '../components/sidebar/MobileNav'
 import SearchBack from '../components/sidebar/SearchBack'
 import '../styles/sidebar/style.css'
 
+
 function Sidebar() {
     return (
-        <div className='sidebar'>
-            <SearchBack/>
-            <Menu/>
-        </div>
+        <>
+            <div className='sidebar'>
+                <SearchBack/>
+                <Menu/>
+            </div>
+            <MobileNav/>
+        </>
     )
 }
 
