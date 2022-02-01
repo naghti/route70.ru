1d2117224bfe6d9cdd37bbd81bb616c2c48b5a96
Rei <you@example.com>
2022-02-01 15:16:19 +0300

    sidebar add additional information

---
+++ src/pages/MobileOtherPlaces.jsx
@@ -0,0 +1,63 @@
+// import React from "react";
+// import '../styles/MobileOtherPlaces/mobileOtherPlaces.css'
+// import Page from "../components/sidebar/Page";
+// function MobileOtherPlaces(props) {
+//     let pages = [
+//         {
+//             text: "Еда",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/cutlery.png",
+//             chapter: 'poest'
+//         },
+//         {
+//             text: "Магазины",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/shopping-bag--v1.png",
+//             chapter: "shinomontag"
+//         },
+//         {
+//             text: "Медицина",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/pill.png",
+//             chapter: "apteki"
+//         },
+//         {
+//             text: "Культура",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/university.png",
+//             chapter: "narodnayaOptika"
+//         },
+//         {
+//             text: "Зона отдыха",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/theme-park.png",
+//             chapter: "avtoservis"
+//         },
+//         {
+//             text: "Образование",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/school.png",
+//             chapter: "shini"
+//         },
+//         {
+//             text: "Соц.помощь",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/about-us-male.png",
+//             chapter: "rubricki"
+//         },
+//         {
+//             text: "Посмотреть все",
+//             img: "https://img.icons8.com/ultraviolet/40/000000/point-objects.png",
+//             chapter: "izbranoe"
+//         }
+//     ];
+//     return (
+//         <div className="menu">
+//             {pages.map((page) => {
+//                 return (
+//                     <Page
+//                         function1={props.function1}
+//                         text={page.text}
+//                         img={page.img}
+//                         chapter={page.chapter}
+//                     />
+//                 );
+//             })}
+//         </div>
+//     );
+// }
+//
+// export default MobileOtherPlaces;
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -27,6 +27,35 @@ function Sidebar(props) {
             <div className={sidebarClassName}>
                 <SearchBack clickOnSearchMarkerOpenInfo={props.clickOnSearchMarkerOpenInfo}  MapOpenMarker={props.MapOpenMarker} openMarker={props.openMarker} />
                 <Menu function1={props.function1} />
+                <div className="otherInfo">
+                    <div className="title">
+                        <h3 className={'titleText'}>Доп.информация</h3>
+                    </div>
+                    <div className="infoBlocksBox">
+                        <div className="infoBlock ">
+                            <div className="infoBlockImgBox">
+                                <img className={'infoBlockImg'} src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/VK_Compact_Logo.svg/190px-VK_Compact_Logo.svg.png" alt="VK"/>
+                            </div>
+                            <h3 className={'infoBlockText'}>Мы в Вконтакте</h3>
+                        </div>
+                        <a href="https://quintadb.ru/widgets/cVW4tcGCjcu4kguSo-W6LZ/cWtJvaWObdOzWTWQ8HWOuD">
+                            <div className="infoBlock ">
+                                <div className="infoBlockImgBox">
+                                    <img className={'infoBlockImg'} src="https://img.icons8.com/ultraviolet/40/000000/feedback.png" alt="VK"/>
+                                </div>
+                                <h3 className={'infoBlockText'}>Обратная Связь</h3>
+                            </div>
+                        </a>
+                        <a href="https://quintadb.ru/widgets/cVW4tcGCjcu4kguSo-W6LZ/blW4RcGvncNioSW7O0ySoC">
+                            <div className="infoBlock ">
+                                <div className="infoBlockImgBox">
+                                    <img className={'infoBlockImg'} src="https://img.icons8.com/ultraviolet/40/000000/add-image.png" alt="VK"/>
+                                </div>
+                                <h3 className={'infoBlockText'}>Добавить Место</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
             <MobileNav 
                 function1={props.function1} 
